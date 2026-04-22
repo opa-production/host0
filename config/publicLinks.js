c2fb16f1d2fae154174bd69b078cd0ba406fd462
Deon62 <orinadeon@gmail.com>
2026-04-22 09:29:37 +0300

    added community link

--- config/publicLinks.js
+++ config/publicLinks.js
@@ -1,4 +1,4 @@
 /**
  * External URLs opened from the app (update when marketing pages are live).
  */
-export const ARDENA_COMMUNITY_URL = 'https://ardena.com/community';
+export const ARDENA_COMMUNITY_URL = 'https://community.ardena.co.ke';
